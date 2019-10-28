e456c7bf0d25cb0d9c0c6643915117009a95364b
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-28 13:47:29 -0700

    writer.js created

--- writer/writer.js
+++ writer/writer.js
@@ -1,13 +1,20 @@
+const fs = require('fs');
 
 function write(str, path) {
-  
+  fs.writeFileSync(path, str);
+  const pathTarget = path.lastIndexOf('/');
+  const result = path.slice(pathTarget + 1, pathTarget.length);
+  console.log(result + ' created');
 }
 
-function writeJSON(obj, path) {
-
+function writeJson(obj, path) {
+  fs.writeFileSync(path, JSON.stringify(obj));
+  const pathTarget = path.lastIndexOf('/');
+  const result = path.slice(pathTarget + 1, pathTarget.length);
+  console.log(result + ' created');
 }
 
 module.exports = {
   write,
-  writeJSON
+  writeJson
 };
