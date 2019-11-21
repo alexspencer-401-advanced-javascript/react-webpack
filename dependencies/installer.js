6a25b56da6c8dcd6da85addb9fcef18887bc5d32
Alex Spencer <alexander.j.spencer@gmail.com>
2019-11-21 11:41:28 -0800

    further logging enhancements

--- dependencies/installer.js
+++ dependencies/installer.js
@@ -1,15 +1,20 @@
 const chalk = require('chalk');
 
 module.exports = function installer(path, packages, dev) {
-  console.log(chalk.greenBright.bold('Installing all packages!'));
   const { execSync } = require('child_process');
   let d;
-  if(dev) d = '-D';
-  else d = '';
+  if(dev) {
+    d = '-D';
+    console.log(chalk.greenBright.bold('Installing Dev Dependencies'));
+  }
+  else {
+    d = '';
+    console.log(chalk.greenBright.bold('Installing Dependencies'));
+  }
 
   execSync(`npm i ${d} ${packages.join(' ')}`, {
     cwd: path,
     stdio: 'inherit'
   });
-  console.log(chalk.greenBright.bold('Package install complete!'));
+  console.log(chalk.greenBright.bold('Package install complete'));
 };
